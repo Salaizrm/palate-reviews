2493af879cc7d74e4e1e32c3200c13120d0e7d0a
Robert Salaiz <work.salaizrm@gmail.com>
2020-09-09 21:56:56 +0000

    added delete and patch

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,19 +1,19 @@
 class ReviewController < ApplicationController
 
-  get '/review/new' do
+  get '/reviews' do
     if Helpers.is_logged_in?(session)
       @user = Helpers.current_user(session)
-      erb :'/review/new'
+      erb :'/review/index'
     else
       flash[:login_error] = erb :'flash_messages/login_error'
       redirect to "/login"
     end
   end
 
-  get '/reviews' do
+  get '/review/new' do
     if Helpers.is_logged_in?(session)
       @user = Helpers.current_user(session)
-      erb :'/review/index'
+      erb :'/review/new'
     else
       flash[:login_error] = erb :'flash_messages/login_error'
       redirect to "/login"
@@ -43,12 +43,52 @@ class ReviewController < ApplicationController
        end
 
   get '/review/:id' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+    @review = Review.find_by(params[:id])
+    erb :'/tweets/show'
+  end
+
+  get '/review/:id/edit' do
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+      @review = Review.find(params[:id])
+    if @review.user == Helpers.current_user(session)
+        erb :'/review/edit'
+    else
+      redirect to '/login'
+    end
   end
 
   patch '/review/:id' do
+    @review = Review.find_by(params[:id])
+    if params[:title] == "" || params[:brand] == "" || params[:type] == "" || params[:rating] == "" || params[:content] == ""
+      redirect to "/review/#{@review.id}/edit"
+    end
+      @review.update(title: params["title"])
+      @review.update(brand: params["brand"])
+      @review.update(type: params["type"])
+      @review.update(rating: params["rating"])
+      @review.update(content: params["content"])
+      @tweet.save
+    redirect to "/tweets/#{@tweet.id}"
   end
 
   delete '/review/:id/delete' do
+    if Helpers.is_logged_in?(session)
+      @review = Review.find(params[:id])
+      if @review.user == Helpers.current_user(session)
+        @review = Review.find_by(params[:id])
+        @review.delete
+        redirect to '/reviews'
+      else
+        redirect to '/reviews'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
 
